658f3f4f8c1d5c2dbecc40e38e48cb47d71183a2
sanaullahpn <sanaullahpn34@gmail.com>
2025-01-03 04:41:05 +0500

    Add files via upload

---
+++ milestone-3 resume/script.js
@@ -0,0 +1,23 @@
+// Get reference to the form and dispaly area
+var form = document.getElementById('resume-from');
+var resumeDispalyElement = document.getElementById('resume-display');
+// handle form submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); //prevent page reload
+    // collect input values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('experience').value;
+    // genrate the resume content dynamically
+    var resumeHtml = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b>".concat(name, "</p>\n     <p><b>Email:</b>").concat(email, "</p>\n      <p><b>Phone:</b>").concat(phone, "</p>\n\n      <h3>Education</h3>\n      <p>").concat(education, "</p>\n\n      <h3>Experience</h3>\n      <p>").concat(experience, "</p>\n\n       <h3>Skills</h3>\n      <p>").concat(skills, "</p>\n    ");
+    // Display the genrated resume
+    if (resumeDispalyElement) {
+        resumeDispalyElement.innerHTML = resumeHtml;
+    }
+    else {
+        console.error('The resme display element is missing.');
+    }
+});
